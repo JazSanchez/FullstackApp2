eb8647116c9aa61e6e6d5365ade0af0f30baa74d
JazSanchez <106107909+JazSanchez@users.noreply.github.com>
2022-08-15 09:54:31 -0700

    authUser

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -66,51 +66,51 @@ export default function CourseDetail() {
     };
 
 
-    // return (
-    //     <div className="wrap">
-    //         <h1>Course Detail</h1>
-    //         <Form
-    //           cancel={cancel}
-    //           errors={errors}
-    //           submit={submit}
-    //           submitButtonText="Update Course"
-    //           elements={() => (
-    //             <React.Fragment>
-    //                <label htmlFor="titile">Course Title</label>
-    //               <input
-    //                 id="titile"
-    //                 name="titile"
-    //                 type="text"
-    //                 defaultValue='' />
-    //                 <label htmlFor="description">Description</label>
-    //               <input
-    //                 id="description"
-    //                 name="description"
-    //                 type="text"
-    //                 defaultValue=''
-    //                 />
-    //                 <label htmlFor="estimatedTime">Estimated Time</label>
-    //                 <input
-    //                 id="estimatedTime"
-    //                 name="estimatedTime"
-    //                 type="text"
-    //                 defaultValue=''
-    //                 />
-    //                 <label htmlFor="materialsNeeded">Materials Needed</label>
-    //               <input
-    //                 id="materialsNeeded"
-    //                 name="materialsNeeded"
-    //                 type="text"
-    //                 defaultValue=''
-    //                  />
-    //             </React.Fragment>
-    //           )} />
-    //         {/* <p>
-    //           Already have a user account? <Link to="/signin">Click here</Link> to sign in!
-    //         </p> */}
-    //       </div>
-    //   );
-    // }
+    return (
+        <div className="main--flex">
+            <h1>Course Detail</h1>
+            <Form
+              cancel={cancel}
+              errors={errors}
+              submit={submit}
+              submitButtonText="Update Course"
+              elements={() => (
+                <React.Fragment>
+                   <label htmlFor="titile">Course Title</label>
+                  <input
+                    id="titile"
+                    name="titile"
+                    type="text"
+                    defaultValue='' />
+                    <label htmlFor="description">Description</label>
+                  <input
+                    id="description"
+                    name="description"
+                    type="text"
+                    defaultValue=''
+                    />
+                    <label htmlFor="estimatedTime">Estimated Time</label>
+                    <input
+                    id="estimatedTime"
+                    name="estimatedTime"
+                    type="text"
+                    defaultValue=''
+                    />
+                    <label htmlFor="materialsNeeded">Materials Needed</label>
+                  <input
+                    id="materialsNeeded"
+                    name="materialsNeeded"
+                    type="text"
+                    defaultValue=''
+                     />
+                </React.Fragment>
+              )} />
+            {/* <p>
+              Already have a user account? <Link to="/signin">Click here</Link> to sign in!
+            </p> */}
+          </div>
+      );
+    }
 
 
       
@@ -139,52 +139,52 @@ export default function CourseDetail() {
     //     })
     // }
 
-    return (
-        <main>
-            <div className="actions--bar">
-                <div className="wrap">
-                    <a className="button" href={`${course.id}/update`}>Update Course</a>
-                    <a className="button"  href="/">Delete Course</a>
-                    <a className="button button-secondary" href="/">Return to List</a>
-                </div>
-            </div>
+    // return (
+    //     <main>
+    //         <div className="actions--bar">
+    //             <div className="wrap">
+    //                 <a className="button" href={`${course.id}/update`}>Update Course</a>
+    //                 <a className="button"  href="/">Delete Course</a>
+    //                 <a className="button button-secondary" href="/">Return to List</a>
+    //             </div>
+    //         </div>
             
-            <div className="wrap" value={id} >
-                <h2>Course Detail</h2>
-                <form >
-                    <div className="main--flex">
-                        <div>
-                            <h3 className="course--detail--title">Course</h3>
-                            <h4 className="course--name">{course.title}</h4>
-                            <p></p>
+    //         <div className="wrap" value={id} >
+    //             <h2>Course Detail</h2>
+    //             <form >
+    //                 <div className="main--flex">
+    //                     <div>
+    //                         <h3 className="course--detail--title">Course</h3>
+    //                         <h4 className="course--name">{course.title}</h4>
+    //                         <p></p>
 
-                            <p></p>
+    //                         <p></p>
              
-                        </div>
-                        <div>
-                            <h3 className="course--detail--title"></h3>
-                            <p>14 hours</p>
+    //                     </div>
+    //                     <div>
+    //                         <h3 className="course--detail--title"></h3>
+    //                         <p>14 hours</p>
 
-                            <h3 className="course--detail--title"></h3>
-                            <ul className="course--detail--list">
-                                <li>1/2 x 3/4 inch parting strip</li>
-                                <li>1 x 2 common pine</li>
-                                <li>1 x 4 common pine</li>
-                                <li>1 x 10 common pine</li>
-                                <li>1/4 inch thick lauan plywood</li>
-                                <li>Finishing Nails</li>
-                                <li>Sandpaper</li>
-                                <li>Wood Glue</li>
-                                <li>Wood Filler</li>
-                                <li>Minwax Oil Based Polyurethane</li>
-                            </ul>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </main>
+    //                         <h3 className="course--detail--title"></h3>
+    //                         <ul className="course--detail--list">
+    //                             <li>1/2 x 3/4 inch parting strip</li>
+    //                             <li>1 x 2 common pine</li>
+    //                             <li>1 x 4 common pine</li>
+    //                             <li>1 x 10 common pine</li>
+    //                             <li>1/4 inch thick lauan plywood</li>
+    //                             <li>Finishing Nails</li>
+    //                             <li>Sandpaper</li>
+    //                             <li>Wood Glue</li>
+    //                             <li>Wood Filler</li>
+    //                             <li>Minwax Oil Based Polyurethane</li>
+    //                         </ul>
+    //                     </div>
+    //                 </div>
+    //             </form>
+    //         </div>
+    //     </main>
 
-    )
+    // )
 
-}
+// }
 
